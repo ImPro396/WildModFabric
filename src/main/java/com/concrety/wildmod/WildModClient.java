e164190a95544eb7b804439a604445d3fc734196
BuildTools <unconfigured@null.spigotmc.org>
2023-12-26 12:07:45 +0100

    Initial commit

---
+++ src/main/java/com/concrety/wildmod/WildModClient.java
@@ -0,0 +1,10 @@
+package com.concrety.wildmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WildModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/concrety/wildmod/WildModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.concrety.wildmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WildModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
