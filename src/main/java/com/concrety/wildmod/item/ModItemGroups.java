913246552f97b0e92ffe0abc3317380c6d1b2304
BuildTools <unconfigured@null.spigotmc.org>
2023-12-26 19:46:08 +0100

    Items

--- src/main/java/com/concrety/wildmod/item/ModItemGroups.java
+++ src/main/java/com/concrety/wildmod/item/ModItemGroups.java
@@ -18,9 +18,11 @@ public class ModItemGroups implements ModInitializer {
                     .entries ((displayContext, entries) -> {
                         entries.add (ModItems.RUBY);
                         entries.add (ModItems.RAW_RUBY);
+                        entries.add (ModItems.METAL_DETECTOR);
 
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
+                        entries.add(ModBlocks.RUBY_ORE);
                     }).build());
 
     public static void registerItemGroups() {
--- src/main/java/com/concrety/wildmod/item/ModItems.java
+++ src/main/java/com/concrety/wildmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.concrety.wildmod.item;
 
 import com.concrety.wildmod.WildMod;
+import com.concrety.wildmod.item.custom.MetalDetectorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -15,6 +16,7 @@ public class ModItems {
 
     public static final Item RUBY = registerItem ("ruby", new Item(new FabricItemSettings ())); //Ruby Item
     public static final Item RAW_RUBY = registerItem ("raw_ruby", new Item(new FabricItemSettings ())); //Raw Ruby Item
+    public static final Item METAL_DETECTOR = registerItem ("metal_detector", new MetalDetectorItem (new FabricItemSettings ().maxDamage (50))); //Raw Ruby Item
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add (RUBY);
         entries.add (RAW_RUBY);
