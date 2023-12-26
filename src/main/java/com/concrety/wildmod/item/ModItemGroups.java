330d83b8b922a71f01b58ae470b56bd9bd2d5c0a
BuildTools <unconfigured@null.spigotmc.org>
2023-12-26 12:56:21 +0100

    Items

---
+++ src/main/java/com/concrety/wildmod/item/ModItemGroups.java
@@ -0,0 +1,32 @@
+package com.concrety.wildmod.item;
+
+import com.concrety.wildmod.WildMod;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups implements ModInitializer {
+
+    public static final ItemGroup MOD_ITEM_GROUP = Registry.register (Registries.ITEM_GROUP, new Identifier (WildMod.MOD_ID, "mod_items"),
+            FabricItemGroup.builder().displayName (Text.translatable ("itemgroup.mod_items")).icon (() -> new ItemStack (ModItems.RUBY))
+                    .entries ((displayContext, entries) -> {
+                        entries.add (ModItems.RUBY);
+                        entries.add (ModItems.RAW_RUBY);
+
+                    }).build());
+
+    public static void registerItemGroups() {
+        WildMod.LOGGER.info ("Registering Itemgroups for " + WildMod.LOGGER);
+    }
+
+    @Override
+    public void onInitialize() {
+        ModItemGroups.registerItemGroups ();
+        ModItems.registerModItems ();
+    }
+}
--- src/main/java/com/concrety/wildmod/item/ModItems.java
+++ src/main/java/com/concrety/wildmod/item/ModItems.java
@@ -1,4 +1,30 @@
 package com.concrety.wildmod.item;
 
+import com.concrety.wildmod.WildMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+
+    public static final Item RUBY = registerItem ("ruby", new Item(new FabricItemSettings ())); //Ruby Item
+    public static final Item RAW_RUBY = registerItem ("raw_ruby", new Item(new FabricItemSettings ())); //Raw Ruby Item
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add (RUBY);
+        entries.add (RAW_RUBY);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register (Registries.ITEM, new Identifier (WildMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        WildMod.LOGGER.info("Registering Mod Items for " + WildMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent (ItemGroups.INGREDIENTS).register (ModItems::addItemsToIngredientItemGroup);
+    }
 }
